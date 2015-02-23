29a0d5376b4eccc1a22229bbed86c8607f1aa3d8
Nagy Bence <nagybence@tipogral.hu>
2015-02-23 12:10:45 +0100

    fixes

--- lib/railhead_autouser.rb
+++ lib/railhead_autouser.rb
@@ -1,4 +1,11 @@
+unless defined?(ActionController::Caching::Sweeper)
+  require 'rails/observers/activerecord/active_record'
+  require 'rails/observers/action_controller/caching'
+end
+
+
 module RailheadAutoUserModel
+
   def self.included(base)
     base.class_eval do
       add_observer(RailheadAutoUserSweeper.instance)
@@ -6,7 +13,9 @@ module RailheadAutoUserModel
   end
 end
 
+
 module RailheadAutoUserController
+
   def auto_user
     class_eval do
       cache_sweeper :railhead_auto_user_sweeper
@@ -14,11 +23,14 @@ module RailheadAutoUserController
   end
 end
 
+
 class RailheadAutoUserSweeper < ActionController::Caching::Sweeper
+
   def before_save(record)
     record.user_id = current_user.id if current_user and record.respond_to?(:user_id) and record.new_record? and record.user_id.nil?
   end
 end
+
 
 ActiveRecord::Base.send :include, RailheadAutoUserModel
 ActionController::Base.send :extend, RailheadAutoUserController
--- railhead_autouser.gemspec
+++ railhead_autouser.gemspec
@@ -1,11 +1,11 @@
 Gem::Specification.new do |s|
   s.name     = "railhead_autouser"
-  s.version  = "0.1.4"
-  s.date     = "2009-10-22"
+  s.version  = "0.1.6"
+  s.date     = "2015-02-23"
   s.summary  = "RailheadAutoUser is a Ruby on Rails plugin that automatically adds current_user to models."
   s.email    = "nagybence@tipogral.hu"
   s.homepage = "http://github.com/nagybence/railhead_autouser"
-  s.description = "RailheadAutoUser is a Ruby on Rails plugin that automatically adds current_user to modelsk."
+  s.description = "RailheadAutoUser is a Ruby on Rails plugin that automatically adds current_user to models."
   s.has_rdoc = true
   s.authors  = ["Bence Nagy"]
   s.files    = ["MIT-LICENSE",
@@ -15,4 +15,5 @@ Gem::Specification.new do |s|
                 "lib/railhead_autouser.rb"]
   s.rdoc_options = ["--main", "README.rdoc"]
   s.extra_rdoc_files = ["README.rdoc"]
+  s.license = 'MIT'
 end
